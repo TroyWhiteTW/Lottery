34d3904274864f16f3f30a799cf34f7d5039f15a
TroyWhiteTW <troywhitetw@ymail.com>
2016-11-18 20:50:09 +0800

    Basic

--- app/src/main/java/com/example/user/lottery/HistoryActivity.java
+++ app/src/main/java/com/example/user/lottery/HistoryActivity.java
@@ -1,13 +1,53 @@
 package com.example.user.lottery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HistoryActivity extends AppCompatActivity {
+    Button btn_history, btn_member, btn_game, btn_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        btn_list = (Button) findViewById(R.id.btn_list_history);
+        btn_game = (Button) findViewById(R.id.btn_game_history);
+        btn_member = (Button) findViewById(R.id.btn_member_history);
+        btn_history = (Button) findViewById(R.id.btn_history_history);
+
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(HistoryActivity.this, ListActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(HistoryActivity.this, GameActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(HistoryActivity.this, MemberActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user/lottery/ListActivity.java
+++ app/src/main/java/com/example/user/lottery/ListActivity.java
@@ -1,13 +1,53 @@
 package com.example.user.lottery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListActivity extends AppCompatActivity {
+    Button btn_history, btn_member, btn_game, btn_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        btn_list = (Button) findViewById(R.id.btn_list_list);
+        btn_game = (Button) findViewById(R.id.btn_game_list);
+        btn_member = (Button) findViewById(R.id.btn_member_list);
+        btn_history = (Button) findViewById(R.id.btn_history_list);
+
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(ListActivity.this, GameActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(ListActivity.this, MemberActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(ListActivity.this, HistoryActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
     }
 }
