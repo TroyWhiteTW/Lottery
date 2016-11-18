e8a1f8fd9d18c1b654c9fdac2cb966c6a1aeeb33
TroyWhiteTW <troywhitetw@ymail.com>
2016-11-18 20:27:37 +0800

    Basic

--- app/src/main/java/com/example/user/lottery/MainActivity.java
+++ app/src/main/java/com/example/user/lottery/MainActivity.java
@@ -7,13 +7,42 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_history;
+    Button btn_history, btn_member, btn_game, btn_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_list = (Button) findViewById(R.id.btn_list_main);
+        btn_game = (Button) findViewById(R.id.btn_game_main);
+        btn_member = (Button) findViewById(R.id.btn_member_main);
         btn_history = (Button) findViewById(R.id.btn_history_main);
+
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, MemberActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
         btn_history.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
