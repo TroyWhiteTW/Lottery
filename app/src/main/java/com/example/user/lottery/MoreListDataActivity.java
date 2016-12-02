c1b0f2f73cd19abea30967ea23c2575d6ea444a0
TroyWhiteTW <troywhitetw@ymail.com>
2016-12-02 16:36:51 +0800

    btn_loadNextPage

--- app/src/main/java/com/example/user/lottery/MoreListDataActivity.java
+++ app/src/main/java/com/example/user/lottery/MoreListDataActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +26,9 @@ public class MoreListDataActivity extends AppCompatActivity {
     private UIHandler handler;
     private UIHandler_2 handler_2;
     private LinearLayout moreOrderList;
+    private TextView tv_totalPages;
+    private Button btn_loadNextPage;
+    private int whatpage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,17 @@ public class MoreListDataActivity extends AppCompatActivity {
         totalPage = it.getIntExtra("totalPage", 1);
         Log.i("troy", cookie);
 
+        tv_totalPages = (TextView) findViewById(R.id.tv_totalPages);
+        tv_totalPages.setText("共 " + totalPage + " 頁");
+
+        btn_loadNextPage = (Button) findViewById(R.id.btn_loadNextPage);
+        btn_loadNextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadNextPage();
+            }
+        });
+
         pDialog = new ProgressDialog(this);
         pDialog.setTitle("Loading Data");
         pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
@@ -47,6 +62,15 @@ public class MoreListDataActivity extends AppCompatActivity {
         moreOrderList = (LinearLayout) findViewById(R.id.moreOrderList);
 
         getData();
+    }
+
+    public void loadNextPage() {
+        if (whatpage < totalPage) {
+            whatpage++;
+            getData();
+        } else {
+            Toast.makeText(this, "已無更多資料", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void getData() {
@@ -63,37 +87,37 @@ public class MoreListDataActivity extends AppCompatActivity {
 
     public void getMoreListData() {
         try {
-            for (int j = 1; j <= totalPage; j++) {
-                String page = String.valueOf(j);
-                MultipartUtility_tw mu = new MultipartUtility_tw("http://mb.sm2.xyz/mobile/wap_ajax.php?action=app_order_dtl");
-                mu.sendCookie(cookie);
-                mu.postKeyValue("page", page);
+//            for (int j = 1; j <= totalPage; j++) {
+            String page = String.valueOf(whatpage);
+            MultipartUtility_tw mu = new MultipartUtility_tw("http://mb.sm2.xyz/mobile/wap_ajax.php?action=app_order_dtl");
+            mu.sendCookie(cookie);
+            mu.postKeyValue("page", page);
 
-                JSONArray ja = mu.getJSONObjectData().getJSONArray("list");
-                int len = ja.length();
-                Log.i("troy", "第" + j + "頁共有" + len + "筆資料");
+            JSONArray ja = mu.getJSONObjectData().getJSONArray("list");
+            int len = ja.length();
+//                Log.i("troy", "第" + j + "頁共有" + len + "筆資料");
 
-                Message msg_2 = new Message();
-                Bundle b_2 = new Bundle();
-                b_2.putString("page", page);
-                msg_2.setData(b_2);
-                handler_2.sendMessage(msg_2);
+            Message msg_2 = new Message();
+            Bundle b_2 = new Bundle();
+            b_2.putString("page", page);
+            msg_2.setData(b_2);
+            handler_2.sendMessage(msg_2);
 
-                for (int i = 0; i < len; i++) {
-                    JSONObject rec = ja.getJSONObject(i);
-                    String number = rec.getString("number");
-                    String money = rec.getString("money");
-                    String frank = rec.getString("frank");
+            for (int i = 0; i < len; i++) {
+                JSONObject rec = ja.getJSONObject(i);
+                String number = rec.getString("number");
+                String money = rec.getString("money");
+                String frank = rec.getString("frank");
 
-                    Message msg = new Message();
-                    Bundle b = new Bundle();
-                    b.putString("number", number);
-                    b.putString("money", money);
-                    b.putString("frank", frank);
-                    msg.setData(b);
-                    handler.sendMessage(msg);
-                }
+                Message msg = new Message();
+                Bundle b = new Bundle();
+                b.putString("number", number);
+                b.putString("money", money);
+                b.putString("frank", frank);
+                msg.setData(b);
+                handler.sendMessage(msg);
             }
+//            }
         } catch (Exception e) {
             Toast.makeText(this, "無法與伺服器取得連線", Toast.LENGTH_LONG).show();
             Log.i("troy", e.toString());
@@ -152,19 +176,16 @@ public class MoreListDataActivity extends AppCompatActivity {
         tv1.setText(number);
         tv1.setTextSize(20);
         tv1.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
-        tv1.setBackgroundColor(Color.parseColor("#ffffff"));
         tv1.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1));
         TextView tv2 = new TextView(MoreListDataActivity.this);
         tv2.setText(money);
         tv2.setTextSize(20);
         tv2.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
-        tv2.setBackgroundColor(Color.parseColor("#ffffff"));
         tv2.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1));
         TextView tv3 = new TextView(MoreListDataActivity.this);
         tv3.setText(frank);
         tv3.setTextSize(20);
         tv3.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
-        tv3.setBackgroundColor(Color.parseColor("#ffffff"));
         tv3.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1));
         ll.addView(tv1);
         ll.addView(tv2);
