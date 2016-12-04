364ee49eab2f2a36d2106c413e010c3c9941e5d8
TroyWhiteTW <troywhitetw@ymail.com>
2016-12-04 21:24:09 +0800

    WinGameActivity

---
+++ app/src/main/java/com/example/user/lottery/WinGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.lottery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WinGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win_game);
+    }
+}
