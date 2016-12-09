24e029f49eb0ed0c2f45e9f55bd5003bb7d12dd0
TroyWhiteTW <troywhitetw@ymail.com>
2016-12-09 15:53:07 +0800

    Qselect

---
+++ app/src/main/java/xyz/sm2/mb/QselectActivity.java
@@ -0,0 +1,84 @@
+package xyz.sm2.mb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QselectActivity extends AppCompatActivity {
+    private Button btn_history, btn_member, btn_game, btn_list, btn_qselect;
+    private String cookie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qselect);
+
+        Intent it = getIntent();
+        cookie = it.getStringExtra("cookie");
+        Log(cookie);
+
+        setFnBtn();
+    }
+
+    public void setFnBtn() {
+        btn_list = (Button) findViewById(R.id.btn_qselect_qselect);
+        btn_game = (Button) findViewById(R.id.btn_game_qselect);
+        btn_member = (Button) findViewById(R.id.btn_member_qselect);
+        btn_history = (Button) findViewById(R.id.btn_history_qselect);
+        btn_qselect = (Button) findViewById(R.id.btn_qselect_qselect);
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(QselectActivity.this, ListActivity.class);
+                it.putExtra("cookie", cookie);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(QselectActivity.this, GameActivity.class);
+                it.putExtra("cookie", cookie);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_qselect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(QselectActivity.this, MemberActivity.class);
+                it.putExtra("cookie", cookie);
+                startActivity(it);
+                finish();
+            }
+        });
+        btn_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(QselectActivity.this, HistoryActivity.class);
+                it.putExtra("cookie", cookie);
+                startActivity(it);
+                finish();
+            }
+        });
+    }
+
+    public void Toast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    public void Log(String s) {
+        Log.i("troy", s);
+    }
+}
