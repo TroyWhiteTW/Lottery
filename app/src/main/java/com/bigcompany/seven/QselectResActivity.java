925148be8ead143bdba0141af7328fcd6957725a
TroyWhiteTW <troywhitetw@ymail.com>
2016-12-10 11:49:49 +0800

    Qselect

---
+++ app/src/main/java/com/bigcompany/seven/QselectResActivity.java
@@ -0,0 +1,13 @@
+package com.bigcompany.seven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QselectResActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qselect_res);
+    }
+}
